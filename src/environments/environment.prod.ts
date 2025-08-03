06f69a5436b2588adc81e80140784697cf7d35b4
Ashutosh mishra <bennyunsigned@gmail.com>
2025-08-03 17:21:33 +0530

    environment added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  appName: 'VidyaRoop',
+  disableRightClick: true,
+  disableDevTools: true,
+  apiUrl: 'https://api.vidyaroop.com',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  appName: 'VidyaRoop',  
+  disableRightClick: false,
+  disableDevTools: false,
+  apiUrl: 'http://localhost:8000',
+};
