9e1d71fae25ef81dc86a212bdf08daecf8864675
Ashutosh Mishra <bennyunsigned@gmail.com>
2025-08-30 17:58:44 +0530

    design changes

--- src/app/components/admin-panel/courses/course-content-details/course-content-details.component.ts
+++ src/app/components/admin-panel/courses/course-content-details/course-content-details.component.ts
@@ -18,15 +18,22 @@ export class CourseContentDetailsComponent implements OnInit {
   loading = true;
   error: string | null = null;
 
-  constructor(
-    private courseProgressService: CourseProgressService,
-    private route: ActivatedRoute
-  ) {}
+  // Pagination properties
+  currentPage = 0;
+  modulesPerPage = 10;
+  totalPages = 0;
+  displayedModules: any[] = [];
+  visiblePages: number[] = [];
 
   ngOnInit(): void {
     this.courseId = Number(this.route.snapshot.paramMap.get('courseId'));
     this.fetchCourseContent();
   }
+
+  constructor(
+    private courseProgressService: CourseProgressService,
+    private route: ActivatedRoute
+  ) {}
 
   fetchCourseContent() {
     this.loading = true;
@@ -34,6 +41,7 @@ export class CourseContentDetailsComponent implements OnInit {
     this.courseProgressService.getPublicCourseContent(this.courseId).subscribe({
       next: (data) => {
         this.courseContent = data;
+        this.setupPagination();
         this.loading = false;
       },
       error: (err) => {
@@ -41,5 +49,55 @@ export class CourseContentDetailsComponent implements OnInit {
         this.loading = false;
       }
     });
+  }
+
+  setupPagination() {
+    const modules = this.courseContent?.Modules || [];
+    this.totalPages = Math.ceil(modules.length / this.modulesPerPage);
+    this.loadCurrentPage();
+    this.visiblePages = this.getVisiblePages();
+  }
+
+  loadCurrentPage() {
+    const modules = this.courseContent?.Modules || [];
+    const startIndex = this.currentPage * this.modulesPerPage;
+    const endIndex = startIndex + this.modulesPerPage;
+    this.displayedModules = modules.slice(startIndex, endIndex);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages - 1) {
+      this.currentPage++;
+      this.loadCurrentPage();
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.loadCurrentPage();
+    }
+  }
+
+  goToPage(page: number) {
+    if (page >= 0 && page < this.totalPages) {
+      this.currentPage = page;
+      this.loadCurrentPage();
+    }
+  }
+
+  onModulesPerPageChange() {
+    this.currentPage = 0;
+    this.setupPagination();
+  }
+
+  getVisiblePages(): number[] {
+    const pages: number[] = [];
+    for (let i = 0; i < this.totalPages; i++) {
+      if (i < 5 || Math.abs(i - this.currentPage) <= 2 || i >= this.totalPages - 3) {
+        pages.push(i);
+      }
+    }
+    return pages;
   }
 }
