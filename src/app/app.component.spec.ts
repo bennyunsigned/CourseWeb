123f0d3fd03f14a189285e9c94d7bbc0716ee005
Ashutosh Mishra <bennyunsigned@gmail.com>
2025-12-06 13:12:07 +0530

    feat: Rename project from ZapLearnAngular to Vidyaroop.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ZapLearn-Angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Vidyaroop');
   });
 });
