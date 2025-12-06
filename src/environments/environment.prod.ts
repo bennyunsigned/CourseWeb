4203b24e729bdeb72efb2ca8bcee0c8ca2121d20
Ashutosh Mishra <bennyunsigned@gmail.com>
2025-12-06 13:27:20 +0530

    feat: Add new PNG carousel images, update carousel component to use them, adjust app name casing, and bump manifest version.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  appName: 'VidyaRoop',
+  appName: 'Vidyaroop',
   disableRightClick: true,
   disableDevTools: true,
   apiUrl: 'https://api.vidyaroop.com',
-  googleAuthKey:'411656733323-2p348igl6kgfo7756i61ivng2b62fkov.apps.googleusercontent.com'
+  googleAuthKey: '411656733323-2p348igl6kgfo7756i61ivng2b62fkov.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  appName: 'VidyaRoop',  
+  appName: 'Vidyaroop',
   disableRightClick: false,
   disableDevTools: false,
   apiUrl: 'http://localhost:8000',
-  googleAuthKey:'411656733323-2p348igl6kgfo7756i61ivng2b62fkov.apps.googleusercontent.com'
+  googleAuthKey: '411656733323-2p348igl6kgfo7756i61ivng2b62fkov.apps.googleusercontent.com'
 };
