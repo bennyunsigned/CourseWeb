60170ba726d9715a2f69f7c66a29a68ed77548c1
bennyunsigned <bennyunsigned@gmail.com>
2026-05-03 00:50:25 +0530

    Delete src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  appName: 'Vidyaroop',
-  disableRightClick: true,
-  disableDevTools: true,
-  apiUrl: 'https://api.vidyaroop.com',
-  googleAuthKey: '411656733323-2p348igl6kgfo7756i61ivng2b62fkov.apps.googleusercontent.com'
-};
